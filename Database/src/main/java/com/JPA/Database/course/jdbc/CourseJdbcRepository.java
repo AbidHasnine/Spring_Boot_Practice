53a633e2a8b45edf82850bac086b176ef5fc338e
AbidHasnine <abidhasnine12@gmail.com>
2026-02-14 17:57:08 +0600

    Springboot CLR

---
+++ Database/src/main/java/com/JPA/Database/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,37 @@
+package com.JPA.Database.course.jdbc;
+
+import com.JPA.Database.course.jdbc.course.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+
+
+    @Autowired
+    private JdbcTemplate SpringJdbcTemplate;
+
+    private static String INSERT_QUERY=
+            """
+                insert into course(id,name,author)
+                values(?,?,?);
+            """;
+
+    private static String DELETE_QUERY=
+            """
+                delete from course where id=?;
+            """;
+
+    public void insert(Course course){
+        SpringJdbcTemplate.update(INSERT_QUERY, course.getId(),
+                course.getName(),
+                course.getAuthor());
+    }
+
+    public void deleteById(long id){
+        SpringJdbcTemplate.update(DELETE_QUERY, id);
+    }
+
+
+}
